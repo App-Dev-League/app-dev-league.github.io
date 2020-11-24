1e4a15e42035612104219c0f18401f06c64b832f
krinetic1234 <56781484+krinetic1234@users.noreply.github.com>
2020-11-24 10:16:38 -0800

    Remove navigation JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,27 +91,27 @@
   }
 
   // Navigation active state on scroll
-  var nav_sections = $('section');
-  var main_nav = $('.nav-menu, #mobile-nav');
+  // var nav_sections = $('section');
+  // var main_nav = $('.nav-menu, #mobile-nav');
 
-  $(window).on('scroll', function () {
-    var cur_pos = $(this).scrollTop() + 200;
+  // $(window).on('scroll', function () {
+  //   var cur_pos = $(this).scrollTop() + 200;
 
-    nav_sections.each(function () {
-      var top = $(this).offset().top,
-        bottom = top + $(this).outerHeight();
+  //   nav_sections.each(function () {
+  //     var top = $(this).offset().top,
+  //       bottom = top + $(this).outerHeight();
 
-      if (cur_pos >= top && cur_pos <= bottom) {
-        if (cur_pos <= bottom) {
-          main_nav.find('li').removeClass('active');
-        }
-        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-      }
-      if (cur_pos < 300) {
-        $(".nav-menu ul:first li:first").addClass('active');
-      }
-    });
-  });
+  //     if (cur_pos >= top && cur_pos <= bottom) {
+  //       if (cur_pos <= bottom) {
+  //         main_nav.find('li').removeClass('active');
+  //       }
+  //       main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+  //     }
+  //     if (cur_pos < 300) {
+  //       $(".nav-menu ul:first li:first").addClass('active');
+  //     }
+  //   });
+  // });
 
 
   // Toggle .header-scrolled class to #header when page is scrolled
